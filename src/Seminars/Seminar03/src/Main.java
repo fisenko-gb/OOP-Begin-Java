a7d6046873321b44f714ff3d7e079d6730145f85
fisenko-gb <fisenko-gb@yandex.ru>
2022-11-29 21:02:25 +0400

    ДЗ Comporator

--- src/Seminars/Seminar03/src/Main.java
+++ src/Seminars/Seminar03/src/Main.java
@@ -1,5 +1,28 @@
+package Seminars.Seminar03.src;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> ar = new ArrayList<>();
+        ar.add(16);
+        ar.add(11111);
+        ar.add(252);
+        ar.add(1450);
+        ar.add(13);
+        ar.add(124);
+
+        System.out.println(ar);
+
+        Comparator modComparator = new SortMod();
+        Collections.sort(ar, modComparator);
+
+        System.out.println(ar);
     }
+
 }
